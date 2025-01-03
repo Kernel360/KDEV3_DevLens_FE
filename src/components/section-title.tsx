6416022ec6270c5152c91dedeedd4c507d3275f6
Shim Jeong ah <35457850+joanShim@users.noreply.github.com>
2025-01-03 16:15:20 +0900

    Feat : 헤딩 및 타이틀 컴포넌트 정의

---
+++ src/components/section-title.tsx
@@ -0,0 +1,11 @@
+import Heading from "./ui/heading";
+
+export default function SectionTitle({ children }: { children: string }) {
+  return (
+    <>
+      <Heading as="h2" className="mb-4">
+        {children}
+      </Heading>
+    </>
+  );
+}
