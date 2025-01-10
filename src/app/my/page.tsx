7b379b19eb2e18ce033e3f77965c731e10aacc96
Shim Jeong ah <35457850+joanShim@users.noreply.github.com>
2025-01-10 21:03:33 +0900

    Feat : 마이페이지 컴포넌트 구성

--- src/app/my/page.tsx
+++ src/app/my/page.tsx
@@ -1,7 +1,24 @@
+import SectionTitle from "@/components/section-title";
+import { Avatar, AvatarFallback, AvatarImage, Button } from "@/components/ui";
+import { Pencil } from "lucide-react";
+
 export default function MyPage() {
   return (
     <>
-      <h1>MyPage</h1>
+      <SectionTitle>마이페이지</SectionTitle>
+      {/* 프로필 사진 */}
+      <div className="flex items-center gap-4">
+        <Avatar className="size-20">
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+        <Button variant="outline" size="icon">
+          <Pencil />
+        </Button>
+      </div>
+      {/* 기본 정보 */}
+      {/* 내 프로젝트 */}
+      {/* 회원 탈퇴 */}
     </>
   );
 }
