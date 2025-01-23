6f8accafd193636cb535f4cf748e36ca50b5e2b1
Shim Jeong ah <35457850+joanShim@users.noreply.github.com>
2025-01-23 14:08:22 +0900

    Design : 버튼 스타일 변경

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui";
 import Link from "next/link";
 
 export default function NotFound() {
@@ -10,12 +11,8 @@ export default function NotFound() {
           </h1>
           <p className="text-gray-500">404 Page Not Found</p>
         </div>
-        <Link
-          href="/projects"
-          className="inline-flex h-10 items-center rounded-md border border-gray-200 bg-white px-8 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
-          prefetch={false}
-        >
-          메인 화면으로 돌아가기
+        <Link href="/projects" prefetch={false}>
+          <Button className="mt-4">메인 화면으로 돌아가기</Button>
         </Link>
       </div>
     </div>
