a33ac7bc9385a6333f19ee5ada531375ae17d394
Shim Jeong ah <35457850+joanShim@users.noreply.github.com>
2025-01-30 00:00:55 +0900

    Feat : 회원 상세 페칭

--- src/app/(protected)/members/_components/member-detail.tsx
+++ src/app/(protected)/members/_components/member-detail.tsx
@@ -1,7 +1,18 @@
-export default function MemberDetail() {
+import { adminMemberApi } from "@/lib/apis/admin/adminMemberApi";
+import { useQuery } from "@tanstack/react-query";
+
+export default function MemberDetail({ id }: { id: number }) {
+  const { data, isLoading } = useQuery({
+    queryKey: ["memberDetail", id],
+    queryFn: () => adminMemberApi.getDetail(id),
+  });
+
+  if (isLoading) return <div>Loading...</div>;
+  if (!data) return null;
+
   return (
     <>
-      <h1>MemberDetail</h1>
+      <h1>MemberDetail {id}</h1>
     </>
   );
 }
