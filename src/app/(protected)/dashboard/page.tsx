6b155cc6bc9e7764a67144a404bd824c80f103aa
Shim Jeong ah <35457850+joanShim@users.noreply.github.com>
2025-02-03 18:21:50 +0900

    Chore : 안내문 삭제

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,5 +1,4 @@
 import Header from "@/components/layout/Header";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Suspense } from "react";
 import { ProjectListSkeleton } from "../../../components/skeleton/project-list-skeleton";
 import ProjectSection from "./_components/project-section";
@@ -8,29 +7,6 @@ import { ErrorBoundary } from "@/components/error/error-boundary";
 export default function Dashboard() {
   return (
     <>
-      <Card>
-        <CardHeader>
-          <CardTitle>구현 완료 기능</CardTitle>
-        </CardHeader>
-        <CardContent className="flex gap-8 text-xs">
-          <ul>
-            <li>로그인/로그아웃</li>
-            <li>내 정보 조회</li>
-          </ul>
-          <ul>
-            <li>계정 목록 조회 - 필터링 x</li>
-            <li>계정 생성</li>
-          </ul>
-          <ul>
-            <li>회사 목록 조회 - 필터링 x</li>
-            <li>회사 생성</li>
-          </ul>
-          <ul>
-            <li>프로젝트 목록 조회 - 필터링 x</li>
-            <li>프로젝트 생성</li>
-          </ul>
-        </CardContent>
-      </Card>
       <Header />
       <ErrorBoundary>
         <Suspense fallback={<ProjectListSkeleton />}>
