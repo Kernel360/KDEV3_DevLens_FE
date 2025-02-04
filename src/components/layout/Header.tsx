d6fe17ff57185bb3bf05a5a71e953750dac67a27
Shim Jeong ah <35457850+joanShim@users.noreply.github.com>
2025-02-04 11:17:40 +0900

    Design : 사이드바 트리거 아이콘 변경

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -14,15 +14,15 @@ interface BreadcrumbItemType {
   href?: string;
 }
 
-type HeaderProps ={
+type HeaderProps = {
   breadcrumbs?: BreadcrumbItemType[];
-}
+};
 
 export default function Header({ breadcrumbs }: HeaderProps) {
   return (
     <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
       <div className="flex items-center gap-2">
-        <SidebarTrigger className="-ml-1" />
+        <SidebarTrigger className="-ml-1 md:hidden" />
         {breadcrumbs && (
           <>
             <Separator orientation="vertical" className="mr-2 h-4" />
--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -3,7 +3,7 @@
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { VariantProps, cva } from "class-variance-authority";
-import { PanelLeft } from "lucide-react";
+import { SquareChevronRight } from "lucide-react";
 
 import { useIsMobile } from "@/hooks/use-mobile";
 import { cn } from "@/lib/utils";
@@ -295,7 +295,7 @@ const SidebarTrigger = React.forwardRef<
       }}
       {...props}
     >
-      <PanelLeft />
+      <SquareChevronRight />
       <span className="sr-only">Toggle Sidebar</span>
     </Button>
   );
