82b47937925ff0ece9e2b583f792f712f154ac52
Shim Jeong ah <35457850+joanShim@users.noreply.github.com>
2025-02-04 14:34:33 +0900

    Chore : 빌드에러 해결

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,18 +1,20 @@
 import Header from "@/components/layout/Header";
-// import { Suspense } from "react";
-// import { ProjectListSkeleton } from "../../../components/skeleton/project-list-skeleton";
-// import ProjectSection from "./_components/project-section";
-// import { ErrorBoundary } from "@/components/error/error-boundary";
+import { Suspense } from "react";
+import { ProjectListSkeleton } from "../../../components/skeleton/project-list-skeleton";
+import ProjectSection from "./_components/project-section";
+import { ErrorBoundary } from "@/components/error/error-boundary";
+
+export const dynamic = "force-dynamic";
 
 export default function DashboardPage() {
   return (
     <>
       <Header />
-      {/* <ErrorBoundary>
+      <ErrorBoundary>
         <Suspense fallback={<ProjectListSkeleton />}>
           <ProjectSection />
         </Suspense>
-      </ErrorBoundary> */}
+      </ErrorBoundary>
     </>
   );
 }
--- src/app/(protected)/my/page.tsx
+++ src/app/(protected)/my/page.tsx
@@ -1,19 +1,21 @@
 import SectionTitle from "@/components/composites/section-title";
 import Header from "@/components/layout/Header";
-import { Card, CardHeader, CardTitle } from "@/components/ui";
-// import FetchAccount from "./_components/fetch-account";
+// import { Card, CardHeader, CardTitle } from "@/components/ui";
+import FetchAccount from "./_components/fetch-account";
+
+export const dynamic = "force-dynamic";
 
 export default function MyPage() {
   return (
     <div className="flex flex-col">
       <Header />
       <SectionTitle>마이페이지</SectionTitle>
-      <Card>
+      {/* <Card>
         <CardHeader>
           <CardTitle>준비중</CardTitle>
         </CardHeader>
-      </Card>   
-      {/* <FetchAccount /> */}
+      </Card>    */}
+      <FetchAccount />
     </div>
   );
 }
