51b2457e47f52a4e825e053bb4b0dfa06401ee3e
Shim Jeong ah <35457850+joanShim@users.noreply.github.com>
2025-02-11 19:51:47 +0900

    Feat : 체크리스트 생성 구현

--- src/app/(protected)/projects/[projectId]/_components/kanban-board.tsx
+++ src/app/(protected)/projects/[projectId]/_components/kanban-board.tsx
@@ -3,9 +3,10 @@
 import { ProjectApi } from "@/lib/apis/main/projectApi";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { Button, Card, CardContent, CardHeader, CardTitle } from "@ui";
-import { ChevronDown, ChevronUp, Plus } from "lucide-react";
+import { ChevronDown, ChevronUp } from "lucide-react";
 import { useState } from "react";
 import AddStepDialog from "./add-step-dialog";
+import CreateChecklistDialog from "./create-checklist-dialog";
 import EditStepDialog from "./edit-step-dialog";
 import IssueCard from "./issue-card";
 
@@ -54,27 +55,21 @@ export function KanbanBoard({ projectId }: { projectId: number }) {
             .sort((a, b) => a.stepOrder - b.stepOrder)
             .map((step) => (
               <div key={step.stepId} className="w-[15rem] shrink-0">
-                <Card className="group h-full border-none bg-zinc-50 px-3 shadow-none *:p-2">
-                  <CardHeader>
+                <Card className="group h-full border-none bg-zinc-50 px-3 shadow-none [&>*]:p-2">
+                  <CardHeader className="!pb-0">
                     <CardTitle
-                      className="flex h-[2.25rem] cursor-pointer items-center justify-between gap-2 text-sm hover:text-primary"
+                      className="flex h-[2.25rem] cursor-pointer items-center justify-between gap-2text-sm hover:text-primary"
                       onClick={() => setIsExpanded(!isExpanded)}
                     >
                       {step.stepName}
                       <EditStepDialog stepInfo={step} />
                     </CardTitle>
                   </CardHeader>
-                  <CardContent className="space-y-2">
+                  <CardContent className="max-h-[40dvh] space-y-2 overflow-y-auto !pt-0 scrollbar-hide">
+                    <CreateChecklistDialog stepId={step.stepId} />
                     {step.projectChecklist.map((issue) => (
                       <IssueCard key={issue.checklistId} issue={issue} />
                     ))}
-                    <Button
-                      variant="outline"
-                      className="invisible flex h-4 w-full items-center justify-center bg-transparent opacity-0 transition-all duration-200 group-hover:visible group-hover:opacity-100"
-                      disabled
-                    >
-                      <Plus size={12} />
-                    </Button>
                   </CardContent>
                 </Card>
               </div>
@@ -99,7 +94,7 @@ export function KanbanBoard({ projectId }: { projectId: number }) {
                       onClick={() => setIsExpanded(!isExpanded)}
                     >
                       <span>{step.stepName}</span>
-                      <span className="border-primary bg-background flex h-5 w-5 items-center justify-center rounded-full border  text-xs">
+                      <span className="flex h-5 w-5 items-center justify-center rounded-full border border-primary bg-background text-xs">
                         {step.projectChecklist.length}
                       </span>
                     </CardTitle>
