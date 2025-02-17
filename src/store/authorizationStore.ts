729db5534f8ed9e48761033da4524c3d0e5b236a
Shim Jeong ah <35457850+joanShim@users.noreply.github.com>
2025-02-17 16:52:29 +0900

    Feat : 프로젝트 내 권한 전역관리

--- src/store/authorizationStore.ts
+++ src/store/authorizationStore.ts
@@ -1,11 +1,11 @@
 import { create } from "zustand";
 
 interface AuthorizationState {
-  authorization: string | null;
+  authorization: string;
   setAuthorization: (auth: string) => void;
 }
 
 export const useAuthorizationStore = create<AuthorizationState>((set) => ({
-  authorization: null,
+  authorization: "",
   setAuthorization: (auth) => set({ authorization: auth }),
 }));
